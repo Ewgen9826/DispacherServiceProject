024e681623c5466210a7c478aa773aa443d139ae
Ewgeny <kuzmich-ewgen@list.ru>
2019-04-26 12:23:49 +0300

    init project

---
+++ DispatcherServiceApp/Views/AddRequestControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows.Controls;
+
+namespace DispatcherServiceApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddRequestControl.xaml
+    /// </summary>
+    public partial class AddRequestControl : UserControl
+    {
+        public AddRequestControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogHost.CloseDialogCommand.Execute(true,btn);
+        }
+    }
+}
---
+++ DispatcherServiceApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DispatcherServiceApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddMainControl(new LoginControl());
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            WindowState=WindowState.Minimized;
+        }
+
+        public void AddMainControl(UserControl control)
+        {
+            main.Children.Clear();
+            main.Children.Add(control);
+        }
+    }
+}
+
